c63bc537de54b38cbb65fbb1fcad4a77113095ce
ZeronP <nik4m.5hreyas@gmail.com>
2020-05-20 19:31:18 +0530

    Added Authentication

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+
+
 const db = require('../models');
 
 
@@ -5,9 +8,13 @@ exports.register = async (req,res,next)=>{
     try{
         const user = await db.User.create(req.body);
         const {id, username} = user;
-        res.json({id,username});
+        const token = jwt.sign({id,username},process.env.SECRET);
+        res.status(201).json({id,username, token});
     }
     catch(err){
+        if(err.code === 11000){
+            err.message = 'Sorry username is already taken.';
+        }
         next(err);
     }
 }
@@ -18,13 +25,15 @@ exports.login = async (req,res,next)=>{
         const {id, username} = user;
         const valid = await user.comparePassword(req.body.password);
         if(valid){
-            res.json({id,username});
+            const token = jwt.sign({id,username},process.env.SECRET);
+            res.json({id,username,token});
         }
         else{
-            throw new Error("Invalid username/password");
+            throw new Error();
         }
     }
-    catch(err){
+    catch(err){ 
+        err.message = "Invalid username/password";
         next(err);
     }
 }
