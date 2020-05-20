14ad3678d1ed2f2160f9f4dbb6b4f1cbe1bab004
ZeronP <nik4m.5hreyas@gmail.com>
2020-05-20 14:39:30 +0530

    Login Registration

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const routes = require('./routes');
 const db = require('./models');
 const handle = require('./handlers');
 const app = express();
@@ -10,8 +10,14 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
+
 app.get('/',(req,res)=>res.json({hello:"World"}))
+app.use('/api/auth',routes.auth);
+
 
 app.use(handle.notFound);
 app.use(handle.errors);
+
+
+
 app.listen(port,console.log(`Server started on port ${port}`));
--- server/models/poll.js
+++ server/models/poll.js
@@ -12,9 +12,13 @@ const pollSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
+    created: {type: Date, default: Date.now},
     question: String,
     options: [optionSchema],
     voted: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
 })
+
+
+
 
 module.exports = mongoose.model('Poll',pollSchema);
