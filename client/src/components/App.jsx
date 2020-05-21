2d213ce5aafef187d87f55ad67b57f89d21ffab3
ZeronP <nik4m.5hreyas@gmail.com>
2020-05-21 19:09:16 +0530

    Api service created

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import api from '../services/api';
 
-const App = () => <div>App works</div>
+
+class App extends React.Component {
+    async componentDidMount(){
+        const result = await api.call('post','auth/login',{
+            username: 'zeronp',
+            password: 'password'
+        });
+        console.log(result);
+    }
+    render(){
+        return <div>App works</div>
+    }
+}
+
 
 export default App;
